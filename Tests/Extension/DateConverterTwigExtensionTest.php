94816b6a99ab840d8dc1ffcf60fc59d4dacb0210
Artur Gajewski <info@arturgajewski.com>
2013-07-03 16:22:55 +0300

    Renamed function to reflect that of extension

--- Tests/Extension/DateConverterTwigExtensionTest.php
+++ Tests/Extension/DateConverterTwigExtensionTest.php
@@ -21,13 +21,13 @@ class DateConverterTwigExtensionTest extends \PHPUnit_Framework_TestCase
     {
         $extension = new DateConverterTwigExtension();
 
-        $this->assertEquals("0 seconds ago", $extension->createdAgo(new \DateTime()));
-        $this->assertEquals("34 seconds ago", $extension->createdAgo($this->getDateTime(-34)));
-        $this->assertEquals("1 minute ago", $extension->createdAgo($this->getDateTime(-60)));
-        $this->assertEquals("2 minutes ago", $extension->createdAgo($this->getDateTime(-120)));
-        $this->assertEquals("1 hour ago", $extension->createdAgo($this->getDateTime(-3600)));
-        $this->assertEquals("1 hour ago", $extension->createdAgo($this->getDateTime(-3601)));
-        $this->assertEquals("2 hours ago", $extension->createdAgo($this->getDateTime(-7200)));
+        $this->assertEquals("0 seconds ago", $extension->ago(new \DateTime()));
+        $this->assertEquals("34 seconds ago", $extension->ago($this->getDateTime(-34)));
+        $this->assertEquals("1 minute ago", $extension->ago($this->getDateTime(-60)));
+        $this->assertEquals("2 minutes ago", $extension->ago($this->getDateTime(-120)));
+        $this->assertEquals("1 hour ago", $extension->ago($this->getDateTime(-3600)));
+        $this->assertEquals("1 hour ago", $extension->ago($this->getDateTime(-3601)));
+        $this->assertEquals("2 hours ago", $extension->ago($this->getDateTime(-7200)));
     }
 
     protected function getDateTime($delta)
